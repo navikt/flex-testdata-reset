fc700485d0cc1efac4028bf624cea143fd3d198a
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-01-03 20:49:22 +0100

    nullstilling

--- src/test/kotlin/no/nav/helse/flex/testdata/ResetTestdataTest.kt
+++ src/test/kotlin/no/nav/helse/flex/testdata/ResetTestdataTest.kt
@@ -15,7 +15,7 @@ class ResetTestdataTest : Testoppsett() {
             .andExpect(status().isOk)
             .andReturn().response.contentAsString
 
-        response `should be equal to` "Resetting av $fnr bestilt hos flex apper fra flex-testdata-reset"
+        response `should be equal to` "Nullstilling av $fnr bestilt hos flex apper fra flex-testdata-reset"
 
         val records = kafkaConsumer.ventPåRecords(antall = 1)
         records shouldHaveSize 1
